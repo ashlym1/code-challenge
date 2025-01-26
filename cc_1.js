ee2a3b91745e4a72b2123cfd4435ada2408a66ce
ashlym1 <ashlym3@usf.edu>
2025-01-26 13:13:12 -0500

    Completed all tasks

--- cc_1.js
+++ cc_1.js
@@ -1,8 +1,8 @@
 // Task 1 - Employee Information
 
-let employeeName = "Yazmin Hue";
-const employeeID = 34904;
-var isActive = true;
+let employeeName = "Yazmin Hue"
+const employeeID = 34904
+var isActive = true
 
 console.log("Employee Name:", employeeName, "- Type:", typeof employeeName);
 console.log("Employee ID:", employeeID, "- Type:", typeof employeeID);
@@ -10,9 +10,9 @@ console.log("Is Active:", isActive, "- Type:", typeof isActive);
 
 // Task 2 - Product Details
 
-let productName = "Strawberry Matcha";
-const productPrice = 10.99;
-var isAvailable = true;
+let productName = "Strawberry Matcha"
+const productPrice = 10.99
+var isAvailable = true
 
 console.log("Product Name:", productName, "- Type:", typeof productName);
 console.log("Product Price:", productPrice, "- Type:", typeof productPrice);
@@ -26,9 +26,9 @@ console.log("Updated Account Balance after purchase and deposit:", accountBalanc
 
 // Task 4 - Customer Messaging
 
-let customerName = "Alice";
-let welcomeMessage = "Hello, " + customerName + "! Welcome to our store.";
-console.log(welcomeMessage);
+let customerName = "Lili"; // Assign customer's name
+let welcomeMessage = "Hello, " + customerName + "! Welcome to our store."; // Concatenate message
+console.log(welcomeMessage); // Log the message to the console
 
 // Task 5 - Access Control
 
