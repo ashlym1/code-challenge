a80ba2c5a8fda0fde0812ec7786d6274f7d68d04
ashlym1 <ashlym3@usf.edu>
2025-01-26 13:07:59 -0500

    Completed all tasks

--- cc_1.js
+++ cc_1.js
@@ -7,6 +7,7 @@ var isActive = true;
 console.log("Employee Name:", employeeName, "- Type:", typeof employeeName);
 console.log("Employee ID:", employeeID, "- Type:", typeof employeeID);
 console.log("Is Active:", isActive, "- Type:", typeof isActive);
+
 // Task 2 - Product Details
 
 let productName = "Strawberry Matcha";
@@ -16,3 +17,26 @@ var isAvailable = true;
 console.log("Product Name:", productName, "- Type:", typeof productName);
 console.log("Product Price:", productPrice, "- Type:", typeof productPrice);
 console.log("Is Available:", isAvailable, "- Type:", typeof isAvailable);
+
+// Task 3 - Financial Transactions
+
+let accountBalance = 50; // starting balance
+accountBalance -= 10.99;  // subtract the cost of the ceremonial grade match ($10.99)
+console.log("Updated Account Balance after purchase and deposit:", accountBalance);
+
+// Task 4 - Customer Messaging
+
+let customerName = "Alice";
+let welcomeMessage = "Hello, " + customerName + "! Welcome to our store.";
+console.log(welcomeMessage);
+
+// Task 5 - Access Control
+
+let isLoggedIn = true;
+let hasAccess = isLoggedIn && true; // Logical AND
+let canEdit = isLoggedIn || false;  // Logical OR
+
+console.log("User Logged In:", isLoggedIn);
+console.log("Has Access:", hasAccess);
+console.log("Can Edit:", canEdit);
+
